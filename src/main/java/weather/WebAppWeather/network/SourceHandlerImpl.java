e84bad35bd755001530b940be4e568d096ed3a3e
André Aguiar <andrefaguiar@upskill.pt>
2021-04-13 18:46:23 +0100

    loading properties problems

--- src/main/java/weather/WebAppWeather/network/SourceHandlerImpl.java
+++ src/main/java/weather/WebAppWeather/network/SourceHandlerImpl.java
@@ -2,6 +2,7 @@ package weather.WebAppWeather.network;
 
 import net.minidev.json.JSONArray;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
@@ -12,31 +13,43 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 @Service
+@ConfigurationProperties(prefix = "weather")
 public class SourceHandlerImpl implements SourceHandler {
 
-    @Value("${application.weather.source.url}")
-    private String baseUrl;
-    @Value("${application.weather.API.key}")
-    private String key;
-    @Value("${application.weather.deafult.city.name}")
-    private String cityDefault;
-    @Value("${application.weather.deafult.city.country}")
-    private String countryDefault;
-    @Value("${application.weather.deafult.unit}")
-    private String unitDefault;
-    @Value("${application.weather.deafult.language}")
-    private String langDefault;
-    @Value("${application.weather.deafult.timesatmps.count}")
-    private int countDeafult;
-    @Value("${application.weather.3days.timesatmps.count}")
-    private int forecast3day;
+    @Value("${weather.source.baseUrl}")
+    private final String baseUrl;
+    @Value("${weather.API.key}")
+    private final String key;
+    @Value("${weather.default.city.nameDefault}")
+    private final String nameDefault;
+    @Value("${weather.default.city.countryDefault}")
+    private final String countryDefault;
+    @Value("${weather.default.unitDefault}")
+    private final String unitDefault;
+    @Value("${weather.default.langDefault}")
+    private final String langDefault;
+    @Value("${weather.default.timestamps.countDefault}")
+    private final int countDefault;
+    @Value("${weather.3days.timestamps.forecast3day}")
+    private final int forecast3day;
+
+    public SourceHandlerImpl(String baseUrl, String key, String nameDefault, String countryDefault, String unitDefault, String langDefault, int countDefault, int forecast3day) {
+        this.baseUrl = baseUrl;
+        this.key = key;
+        this.nameDefault = nameDefault;
+        this.countryDefault = countryDefault;
+        this.unitDefault = unitDefault;
+        this.langDefault = langDefault;
+        this.countDefault = countDefault;
+        this.forecast3day = forecast3day;
+    }
 
     public String getURL(String name, String country, int days){
         String cityName = name;
         String countryCod = "," + country;
-        int nrCount = countDeafult;
+        int nrCount = countDefault;
         if (name == null ){
-            cityName = cityDefault;
+            cityName = nameDefault;
             countryCod = "," + countryDefault;
         }
 
